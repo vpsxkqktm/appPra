7150eda9cff50418f3edb6fcbc1835cb00962fb4
vpsxkqktm <sd1593@gmail.com>
2022-06-03 00:34:21 +1200

    swiper and banner ads

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View   } from 'react-native';
+import { StyleSheet, Text, View, Platform   } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Home from './scr/navbar/Home';
 import  Settings  from './scr/navbar/Settings'
 import { NavigationContainer } from '@react-navigation/native';
+import {
+  AdMobBanner,
+  AdMobInterstitial,
+  PublisherBanner,
+  AdMobRewarded,
+  setTestDeviceIDAsync,
+} from 'expo-ads-admob';
+
+
+
+
 
 const Tab = createMaterialTopTabNavigator();
 export default function App() {
+  let adunitId =  Platform.select({
+    ios:"ca-app-pub-3940256099942544/2934735716",
+    android: "ca-app-pub-3940256099942544/2934735716"
+  });
+  let loadAd = async () => {
+    await AdMobBanner.setAd
+  };
   return (
     <SafeAreaView style={styles.container}>
         <NavigationContainer>
@@ -17,6 +35,11 @@ export default function App() {
             <Tab.Screen name="Settings" component={Settings} />
           </Tab.Navigator>
         </NavigationContainer>
+        <AdMobBanner
+          bannerSize="fullBanner"
+          adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+          servePersonalizedAds // true or false
+          onDidFailToReceiveAdWithError={"The banner does not work properly"} />
       </SafeAreaView>
   );
 }
--- scr/navbar/Home.js
+++ scr/navbar/Home.js
@@ -1,15 +1,33 @@
 import * as React from 'react';
 import { StyleSheet, Text, View  } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Swiper from 'react-native-swiper'
+
+
 
 export default function Home() {
     return(
     <SafeAreaView style={styles.container}>
-        <Text>Hi!</Text>
+        <View style={SizeSwiper}>
+            <Swiper style={styles2.wrapper} showsButtons={true}>
+                <View style={styles2.slide1}>
+                <Text style={styles2.text}>Hello Swiper</Text>
+                </View>
+                <View style={styles2.slide2}>
+                <Text style={styles2.text}>Beautiful</Text>
+                </View>
+                <View style={styles2.slide3}>
+                <Text style={styles2.text}>And simple</Text>
+            </View>
+        </Swiper>
+      </View>
     </SafeAreaView>
     );
 }
 
+const SizeSwiper = StyleSheet.create({
+    flex: 0.25,
+});
 const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -17,3 +35,29 @@ const styles = StyleSheet.create({
     },
   });
   
+const styles2 = StyleSheet.create({
+wrapper: {},
+slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+},
+slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+},
+slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+},
+text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+}
+})
